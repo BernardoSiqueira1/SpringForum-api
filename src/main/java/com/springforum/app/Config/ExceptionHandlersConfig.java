948fc7b3518f377b04fda2a8333bcd2b40ee5f60
BernardoSiqueira1 <bernardoasiqueira@hotmail.com>
2025-04-11 17:43:59 -0300

    feat: adicionado configuração para exception handlers

---
+++ src/main/java/com/springforum/app/Config/ExceptionHandlersConfig.java
@@ -0,0 +1,9 @@
+package com.springforum.app.Config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@Configuration
+@ControllerAdvice
+public class ExceptionHandlersConfig {
+}
