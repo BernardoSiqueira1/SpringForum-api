b189175e5f363a5a35569af46f14ef41f34a4a37
BernardoSiqueira1 <bernardoasiqueira@hotmail.com>
2025-04-16 22:36:27 -0300

    chore: adicionado classes de configuração adicionais.

--- src/main/java/com/springforum/app/Config/SecurityConfig.java
+++ src/main/java/com/springforum/app/Config/SecurityConfig.java
@@ -2,9 +2,13 @@ package com.springforum.app.Config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -26,6 +30,16 @@ public class SecurityConfig {
                                 .permitAll()) // Temporário
 
                 .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 
 }
