13311f715cc511c4d159d7d62694b0b8c6c2c2eb
BernardoSiqueira1 <bernardoasiqueira@hotmail.com>
2025-04-16 22:35:34 -0300

    feat: adicionado exception handlers para erros em serviços

--- src/main/java/com/springforum/app/Config/ExceptionHandlersConfig.java
+++ src/main/java/com/springforum/app/Config/ExceptionHandlersConfig.java
@@ -1,9 +1,47 @@
 package com.springforum.app.Config;
 
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.validation.ConstraintViolationException;
+import jakarta.validation.ValidationException;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+import javax.naming.AuthenticationException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @ControllerAdvice
 public class ExceptionHandlersConfig {
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<?> handleInvalidParametersException(){
+        return ResponseEntity.status(400).body("Erro nos parâmetros do caminho da requisição, verifique e tente novamente.");
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleValidationException(MethodArgumentNotValidException validationException){
+        List<String> errorList = validationException.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(error -> error.getField() + ": " + error.getDefaultMessage())
+                .toList();
+
+        return ResponseEntity.status(400).body("Erro nos parâmetros do corpo da requisição: \n" + errorList);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<?> handleNotFoundException(EntityNotFoundException notFoundException){
+        return ResponseEntity.status(404).body(notFoundException.getMessage());
+    }
+
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity<?> handleAuthenticationError(){
+        return ResponseEntity.status(401).body("Credenciais inválidas, verifique usuário/senha");
+    }
+
 }
