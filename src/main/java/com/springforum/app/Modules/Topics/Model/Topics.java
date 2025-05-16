703a4395968252d10b9099332673f94f621e8b87
BernardoSiqueira1 <bernardoasiqueira@hotmail.com>
2025-05-16 19:45:03 -0300

    feat: adicionado entidade topics

---
+++ src/main/java/com/springforum/app/Modules/Topics/Model/Topics.java
@@ -0,0 +1,33 @@
+package com.springforum.app.Modules.Topics.Model;
+
+import com.springforum.app.Modules.Posts.Models.Post;
+import com.springforum.app.Modules.Topics.DTOs.NewTopicDTO;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.Cascade;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Topics {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long topicId;
+
+    @Column(nullable = false, unique = true)
+    private String topicName;
+
+    @JoinColumn
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+    List<Post> relatedPosts;
+
+    public Topics(NewTopicDTO newTopicDTO){
+        this.topicName = newTopicDTO.topicName();
+    }
+}
