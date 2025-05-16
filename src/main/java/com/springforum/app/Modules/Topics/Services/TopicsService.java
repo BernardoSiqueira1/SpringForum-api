bc31a87fb1e6fc8b885d74b9bc33a4af6d1848a2
BernardoSiqueira1 <bernardoasiqueira@hotmail.com>
2025-05-16 19:45:30 -0300

    feat: adicionado service para a lógica dos topics

---
+++ src/main/java/com/springforum/app/Modules/Topics/Services/TopicsService.java
@@ -0,0 +1,43 @@
+package com.springforum.app.Modules.Topics.Services;
+
+import com.springforum.app.Modules.Topics.Adapters.TopicAdapters;
+import com.springforum.app.Modules.Topics.DTOs.GetTopicDTO;
+import com.springforum.app.Modules.Topics.DTOs.NewTopicDTO;
+import com.springforum.app.Modules.Topics.Model.Topics;
+import com.springforum.app.Modules.Topics.Repository.TopicsRepository;
+import jakarta.transaction.Transactional;
+import org.hibernate.query.Page;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TopicsService {
+
+    @Autowired
+    private TopicsRepository topicsRepository;
+
+    @Transactional
+    public void createNewTopic(NewTopicDTO newTopicDTO){
+
+        Topics newPostTopic = new Topics(newTopicDTO);
+
+        topicsRepository.save(newPostTopic);
+
+    }
+
+    public List<GetTopicDTO> getTopicsPage(int pageNumber){
+        Pageable queryPage = PageRequest.of(pageNumber, 5);
+
+        return TopicAdapters.toTopicDTOList(topicsRepository.findAll(queryPage));
+    }
+
+    @Transactional
+    public void deleteExistingTopic(long topicId){
+        topicsRepository.deleteById(topicId);
+    }
+
+}
