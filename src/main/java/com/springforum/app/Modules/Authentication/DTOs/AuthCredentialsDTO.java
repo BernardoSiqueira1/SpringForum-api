5fccac4cd385f1e4b349c6119f961cb0e69d551b
BernardoSiqueira1 <bernardoasiqueira@hotmail.com>
2025-07-28 20:49:03 -0300

    refactor: small folder and structure refactor

--- src/main/java/com/springforum/app/Modules/Authentication/DTOs/AuthCredentialsDTO.java
+++
@@ -1,6 +0,0 @@
-package com.springforum.app.Modules.Authentication.DTOs;
-
-public record AuthCredentialsDTO(
-        String userLogin,
-        String userPassword
-){}
--- src/main/java/com/springforum/app/Modules/User/Enums/UserType.java
+++
@@ -1,6 +0,0 @@
-package com.springforum.app.Modules.User.Enums;
-
-public enum UserType {
-    ADMIN,
-    USER
-}
